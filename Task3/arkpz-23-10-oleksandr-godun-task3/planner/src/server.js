8991598fca008ea84185d5ec909f4f2ed5fa6054
NureHodunOleksandr <oleksandr.hodun@nure.ua>
2025-11-30 19:13:07 +0200

    Delete Task3/arkpz-23-10-oleksandr-godun-task3 directory

--- Task3/arkpz-23-10-oleksandr-godun-task3/planner/src/server.js
+++
@@ -1,9 +0,0 @@
-import open from "open"; // <-- додай імпорт угорі (новий пакет)
-import app from "./app.js";
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
-  open(`http://localhost:${PORT}`); // <-- автоматично відкриває браузер
-});
